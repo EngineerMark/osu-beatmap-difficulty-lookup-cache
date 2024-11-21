93a4d83f2d81be65c082700e3f4954fca83c2375
Mark <engineermarkpro@gmail.com>
2024-11-21 16:44:52 +0100

    testing some parallelization

--- BeatmapDifficultyLookupCache/Controllers/AttributesController.cs
+++ BeatmapDifficultyLookupCache/Controllers/AttributesController.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using osu.Game.Rulesets.Difficulty;
@@ -18,7 +19,42 @@ namespace BeatmapDifficultyLookupCache.Controllers
             this.cache = cache;
         }
 
-        [HttpPost]
-        public Task<DifficultyAttributes> Post([FromBody] DifficultyRequest request) => cache.GetAttributes(request);
+        //[HttpPost]
+        //public Task<DifficultyAttributes> Post([FromBody] DifficultyRequest request)
+        //{
+        //    return cache.GetAttributes(request);
+        //}
+
+        private readonly int AsyncProcesses = 8;
+
+        [HttpPost] //allow 2nd endpoint for multiple requests
+        public async Task<DifficultyAttributes[]> Post([FromBody] DifficultyRequest[] requests)
+        {
+            //return await Task.WhenAll(requests.Select(cache.GetAttributes));
+
+            //Run them async
+
+            var tasks = new Task<DifficultyAttributes>[requests.Length];
+            var results = new DifficultyAttributes[requests.Length];
+
+            for (int i = 0; i < requests.Length; i++)
+            {
+                tasks[i] = cache.GetAttributes(requests[i]);
+            }
+
+            for (int i = 0; i < tasks.Length; i += AsyncProcesses)
+            {
+                await Task.WhenAll(tasks.Skip(i).Take(AsyncProcesses).ToArray());
+            }
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                results[i] = tasks[i].Result;
+            }
+
+            return results;
+        }
+
+        //public Task<DifficultyAttributes> Post([FromBody] DifficultyRequest request) => cache.GetAttributes(request);
     }
 }
